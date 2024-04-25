6c5f6ffffe850816f818dc878baf2ba3c1878af0
Agustin <esparta.01.af@gmail.com>
2024-04-25 19:22:35 -0300

    First backend implementation

---
+++ api/src/models/Type.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('Type', {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false
+        }
+    }, { timestamps: false });
+};
